7631d64c149fcac5a724efefe8734c83c89948f7
Jord <jord018@users.noreply.github.com>
2026-09-20 21:42:29 +0700

    1.7

--- src/main/java/com/example/demo/dao/EventDaoDbImpl.java
+++ src/main/java/com/example/demo/dao/EventDaoDbImpl.java
@@ -29,7 +29,7 @@ public class EventDaoDbImpl implements EventDao {
 
     @Override
     public Page<Event> getEvents(String title, Pageable page) {
-        return eventRepository.findByTitleContainingOrDescriptionContaining(title, title, page);
+        return eventRepository.findByTitleContainingAndDescriptionContaining(title, title, page);
     }
 
     @Override
